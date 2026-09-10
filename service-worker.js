8d9d7c1c7a0b27db6e9e6f615202e5953cbbb75d
katy0409 <b900543@gmail.com>
2026-09-10 13:45:13 +0800

    commit

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='countdown-v5';
+const CACHE='countdown-v5.1';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('countdown-')&&k!==CACHE).map(k=>caches.delete(k))))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
